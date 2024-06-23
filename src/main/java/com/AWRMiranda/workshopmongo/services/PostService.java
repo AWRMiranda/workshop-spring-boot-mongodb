2f49d2cad0f45cf8c78340718f306f30fc647acd
AWRMiranda <alexandrewrm@gmail.com>
2024-06-23 17:06:43 -0300

    Consulta simples com @query

--- src/main/java/com/AWRMiranda/workshopmongo/services/PostService.java
+++ src/main/java/com/AWRMiranda/workshopmongo/services/PostService.java
@@ -24,8 +24,7 @@ public class PostService {
 		return user;
 	}
 
-
-public List<Post> findByTitle(String text) {
-	return repo.findByTitleContainingIgnoreCase(text);
-}
+	public List<Post> findByTitle(String text) {
+		return repo.searchTitle(text);
+	}
 }
